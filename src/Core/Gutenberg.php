0c5bea8554c3ab833698c850948ba0e66213a5cb
Michał Świątek <michal.swiatek@netivo.pl>
2025-04-17 17:08:59 +0200

    Gutenberg load from json

--- src/Core/Gutenberg.php
+++ src/Core/Gutenberg.php
@@ -13,8 +13,8 @@ namespace Netivo\Core;
 use ReflectionClass;
 
 if ( ! defined( 'ABSPATH' ) ) {
-    header( 'HTTP/1.0 403 Forbidden' );
-    exit;
+	header( 'HTTP/1.0 403 Forbidden' );
+	exit;
 }
 
 /**
@@ -24,142 +24,53 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 abstract class Gutenberg {
 
-    /**
-     * Id of the block.
-     *
-     * @var string
-     */
-    protected string $id;
-
-    /**
-     * Handle/namespace of the block.
-     *
-     * @var string
-     */
-    protected string $handle;
-
-    protected array $dependencies = [ 'wp-element', 'wp-blocks' ];
-
-    /**
-     * Callback name.
-     *
-     * @var string|null
-     */
-    protected ?string $callback = null;
-
-    /**
-     * Path to Admin folder on server.
-     *
-     * @var string
-     */
-    protected string $path = '';
-
-    /**
-     * Uri to Admin folder.
-     *
-     * @var string
-     */
-    protected string $uri = '';
-
+	/**
+	 * Callback name.
+	 *
+	 * @var string|null
+	 */
+	protected ?string $callback = null;
+	
 
 	/**
-	 * Front end style to enqueue for block.
-	 * Array format:
-	 * [
-	 *  'file' => #file path to enqueue,
-	 *  'uri' => #file uri to enqueue
-	 * ]
-	 *
-	 * @var null|array
+	 * Gutenberg constructor.
 	 */
-    protected ?array $style = null;
+	public function __construct() {
+		add_action( 'init', [ $this, 'register_block' ] );
+	}
 
 	/**
-	 * Front end script to enqueue for block
-	 * Array format:
-	 * [
-	 *  'file' => #file path to enqueue,
-	 *  'uri' => #file uri to enqueue
-	 * ]
+	 * Registers scripts, styles and block.
 	 *
-	 * @var null|array
+	 * @throws \Exception When error.
 	 */
-    protected ?array $script = null;
-
-    /**
-     * Gutenberg constructor.
-     *
-     * @param string $path Path to Admin folder.
-     * @param string $uri Uri to Admin folder.
-     */
-    public function __construct( string $path, string $uri ) {
-        $this->path = $path;
-        $this->uri  = $uri;
-        $this->init_style();
-        add_action( 'init', [ $this, 'register_block' ] );
-    }
-
-    /**
-     * Registers scripts, styles and block.
-     *
-     * @throws \Exception When error.
-     */
-    public function register_block(): void {
-        $obj      = new ReflectionClass( $this );
-        $filename = $obj->getFileName();
-        $filename = str_replace( '.php', '', $filename );
-        $obj = new ReflectionClass($this);
-        $data = $obj->getAttributes();
-        foreach($data as $attribute) {
-	        if($attribute->getName() == 'Netivo\Attributes\Block') {
+	public function register_block(): void {
+		$obj  = new ReflectionClass( $this );
+		$data = $obj->getAttributes();
+		foreach ( $data as $attribute ) {
+			if ( $attribute->getName() == 'Netivo\Attributes\Block' ) {
 				$name = $attribute->getArguments()[0];
-	        }
-        }
-        if(empty($name)){
-	        $filename = $obj->getFileName();
-	        $filename = str_replace( '.php', '', $filename );
-	        $name = basename($filename);
-	        $name = 'src/views/gutenberg/'.strtolower($name);
-        }
-        $css  = get_template_directory() . '/' . strtolower( $name ) . '/block.css';
-        $css_uri  = get_template_directory_uri() . '/' . strtolower( $name ) . '/block.css';
-        $js   = get_template_directory() . '/' . strtolower( $name ) . '/block.js';
-        $js_uri   = get_template_directory_uri() . '/' . strtolower( $name ) . '/block.js';
+			}
+		}
+		if ( empty( $name ) ) {
+			$filename = $obj->getFileName();
+			$filename = str_replace( '.php', '', $filename );
+			$name     = basename( $filename );
+			$name     = 'src/views/gutenberg/' . strtolower( $name );
+		}
 
-        if ( file_exists( $css ) ) {
-            wp_register_style( $this->handle, $css_uri, array( 'wp-edit-blocks' ) );
-        }
-        if ( file_exists( $js ) ) {
-            wp_register_script( $this->handle, $js_uri, $this->dependencies );
-        } else {
-            throw new \Exception( 'Block js not found.' );
-        }
+		$block_json = get_template_directory() . '/' . strtolower( $name ) . '/block.json';
+		if ( file_exists( $block_json ) ) {
+			$args = [];
+			if ( ! empty( $this->callback ) ) {
+				$args['render_callback'] = array( $this, $this->callback );
+			}
 
-        $args = [
-	        'editor_script' => $this->handle,
-        ];
-        if ( file_exists( $css ) ) {
-	        $args['editor_style'] = $this->handle;
-        }
-        if(!empty($this->style)) {
-	        if (file_exists($this->style['file'])) {
-		        wp_register_style($this->handle . '-style', $this->style['uri']);
-		        $args['style'] = $this->handle . '-style';
-	        }
-        }
-        if(!empty($this->script)) {
-	        if (file_exists($this->script['file'])) {
-		        wp_register_script($this->handle . '-script', $this->script['uri']);
-		        $args['script'] = $this->handle . '-script';
-	        }
-        }
-        if ( ! empty( $this->callback ) ) {
-	        $args['render_callback'] = [ $this, $this->callback ];
-        }
+			register_block_type( $block_json, $args );
+		} else {
+			throw new \Exception( 'Block json not found.' );
+		}
 
-        register_block_type( $this->id, $args );
-
-    }
-    abstract function init_style();
+	}
 
 }
